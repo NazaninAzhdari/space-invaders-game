dce53499a68bf3ad848bc3876d44a81a007924c5
Naz <Nazanin.Azhdari.sbu@gmail.com>
2026-05-18 20:13:22 +0300

    designing UART RX  for communication with fpga

---
+++ UART_RX.vhd
@@ -0,0 +1,89 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity UART_RX is
+    generic (
+        g_BITS_LIMIT        :   integer    :=8;      --can be set to 7 or 8
+        g_CLKS_PER_BIT      :   integer    :=434     --can be determined based on the CLK and Baud rate. =>  CLK / Baud rate
+    );
+    port (
+        i_clk                :   in      STD_LOGIC;
+        i_data_serial        :   in      STD_LOGIC;
+        o_data_parallel      :   out     unsigned(g_BIT_LIMIT -1 downto 0);
+        o_data_DV            :   out     STD_LOGIC
+    );
+end UART_RX;
+
+architecture RTL of UART_RX is
+    
+    type t_UART_SM is (IDLE, START_BIT, RECIEVE_BITS, STOP_BIT);
+    signal   r_SM                 :  t_UART_SM      :=IDLE;
+    constant c_HALF_CLKS_PER_BIT  :  integer        :=(g_CLKS_PER_BIT / 2);
+
+    signal r_clk_counter     :  integer range 0 to g_CLKS_PER_BIT       :=0;
+    signal r_bit_counter     :  integer range 0 to g_BIT_LIMIT          :=0;
+    signal r_data_serial     :  STD_LOGIC                               :='0';
+    signal r_shift           :  unsigned(g_BIT_LIMIT -1 downto 0)       :=(others=>'0');
+    
+    begin
+        process(i_clk) is
+            begin
+                if rising_edge(i_clk) then
+                    r_data_serial <= i_dtat_serial;
+
+                    case r_SM is 
+                        when IDLE =>
+                            r_DV <= '0';
+                            r_clk_counter <= 0;
+                            r_bit_counter <= 0;
+
+                            if i_data_serial = '0' and r_data_serial = '1' then
+                                r_SM <= START_BIT;
+                            end if;
+
+                        when START_BIT =>
+                            if r_clk_counter < c_HALF_CLKS_PER_BIT then
+                                r_clk_counter <= r_clk_counter + 1;
+                            else
+                                r_clk_counter <= 0;
+                                if i_data_serial = '0' then
+                                    r_SM <= RECIEVE_BITS;
+                                else
+                                    r_SM <= IDLE;
+                                end if;
+                            end if;
+
+                        when RECIEVE_BITS =>
+                            if r_clk_counter < g_CLKS_PER_BIT then
+                                r_clk_counter <= r_clk_counter + 1;
+                            else
+                                r_clk_counter <= 0;
+                                if r_bit_counter < BIT_LIMIT then
+                                    r_shift <= i_data_serial & r_shift(g_BITS_LIMIT -1 downto 1);
+                                    r_bit_counter <= r_bit_counter + 1;
+                                else
+                                    r_bit_counter <= 0;
+                                    r_SM <= STOP_BIT;
+                                end if;
+                            end if;
+
+                        when STOP_BIT =>
+                            if i_data_serial = '1' then
+                                r_SM <= IDLE;
+                                r_DV <= '1';
+                            else
+                                r_SM <= IDLE;
+                            end if;
+
+                        when others =>
+                            r_SM <= IDLE;
+
+                        end case;
+                    end if;
+            end process;
+            
+            o_DV <= r_DV;
+            o_data_parallel <= r_shift;
+
+    end RTL;
